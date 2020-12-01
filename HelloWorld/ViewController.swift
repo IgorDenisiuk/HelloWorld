e024178a6b7df2cc11a80f3d72b04e6dbcb0dc4a
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2020-12-01 11:47:02 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Igor Denisiuk on 12/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
